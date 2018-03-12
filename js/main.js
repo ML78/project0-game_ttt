b3c99bce97d1c6ff5102b82d8bdabbe541118b2b
Monique Lee <moniquelee.7878@gmail.com>
2018-03-13 10:15:38 +1100

    2nd cut - day 2

--- js/main.js
+++ js/main.js
@@ -3,81 +3,64 @@ const renderBoard = function () {
     $('td').addClass('box');
   };
 
-
 $(document).ready(function() {
-
   renderBoard();
 
-
-/*  Should make this the selector class instead of using id#crosses and id#noughts.
+/*
+//  Should make this the selector class instead of using id#crosses and id#noughts.
 // Could make the player = this. find closest i? couldn't get it to work!
-
-returns undefined
 $('.selector').on('click', function(){
-  let player1 = $(this).closest('i').find().slidedown;
-  console.log(player1);
+  let $startPlayer = $(this).closest('i').find().slidedown;
+  console.log($startPlayer);
 });
 */
 
   let playerX = "X";
   let playerO = "O";
+  let startPlayer = "";
+  let nextPlayer = "";
 
-    $('#crosses').on('click', function(){
-      //alert('X clicked');
-      //update turn
-      let player1 = playerX;
-      $('span').html(player1);
+  $('#crosses').on('click', function(){
+      startPlayer = playerX;
+      $('span').text(startPlayer);
+      return startPlayer;
     });
 
     $('#noughts').on('click', function(){
-      //alert('O clicked');
-      //update turn
-      let player1 = playerO;
-      $('span').html(player1);
+      startPlayer = playerO;
+      $('span').text(startPlayer);
+      return startPlayer;
     });
-
-// let player = document.getElementById('player').innerHTML;
-// console.log(player);
 
 // stop it from proceeding if the X or O is not selected first?
 
-let player = playerX;
+  let player = "";
+
+  // player = $('span').text();
+  // console.log(player); //this isn't working !!!
+
+  if (startPlayer === playerX) {
+    player = playerX; // IT'S NOT PICKING THIS UP
+  } else {
+        console.log(startPlayer);
+    player = playerO;
+  }
+
+//let player = playerX; //THIS WORKS BECAUSE IT'S HARD-CODED
 
 if (player){
-$('#aa').on('click', function(){
+$('.box').on('click', function(){
     $(this).text(player);
-});
-$('#ab').on('click', function(){
-    $(this).text(player);
-});
-$('#ac').on('click', function(){
-    $(this).text(player);
-});
-$('#ba').on('click', function(){
-    $(this).text(player);
-});
-$('#bb').on('click', function(){
-    $(this).text(player);
-});
-$('#bc').on('click', function(){
-    $(this).text(player);
-});
-$('#ca').on('click', function(){
-    $(this).text(player);
-});
-$('#cb').on('click', function(){
-    $(this).text(player);
-});
-$('#cc').on('click', function(){
-    $(this).text(player);
-});
+  });
 }
+
+
+//toggle between X & O in span to change players?
 
 /*
   $('td').on('click', function(){
-     $('#cross').toggle();
-     $('#nought').toggle();
-   });
+     $('#player').toggle();
+     });
 */
 
 
