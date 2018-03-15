62bffdd12266537a9aba86b9b4a050223cd002ef
Monique Lee <moniquelee.7878@gmail.com>
2018-03-15 14:23:10 +1100

    10th cut - day 4

--- js/main.js
+++ js/main.js
@@ -55,35 +55,35 @@ if (startPlayer){
           for(i=0; i<gameBoard.length; i++){
             if(gameBoard[0]!=null && gameBoard[0]===gameBoard[1] && gameBoard[1]===gameBoard[2]){
                 winner = gameBoard[0];
-                winnerMessage += `The winner is: ${winner}`;
+                winnerMessage += `Winner: ${winner}`;
                 $('.turn').addClass('ghost');
             } else if(gameBoard[3]!=null && gameBoard[3]===gameBoard[4] && gameBoard[4]===gameBoard[5]){
                 winner = gameBoard[3];
-                winnerMessage += `The winner is: ${winner}`;
+                winnerMessage += `Winner: ${winner}`;
                 $('.turn').addClass('ghost');
             } else if(gameBoard[6]!=null && gameBoard[6]===gameBoard[7] && gameBoard[7]===gameBoard[8]){
                 winner = gameBoard[6];
-                winnerMessage += `The winner is: ${winner}`;
+                winnerMessage += `Winner: ${winner}`;
                 $('.turn').addClass('ghost');
             } else if (gameBoard[0]!=null && gameBoard[0]===gameBoard[3] && gameBoard[3]===gameBoard[6]){
                 winner = gameBoard[0];
-                winnerMessage += `The winner is: ${winner}`;
+                winnerMessage += `Winner: ${winner}`;
                 $('.turn').addClass('ghost');
             } else if (gameBoard[1]!=null && gameBoard[1]===gameBoard[4] && gameBoard[4]===gameBoard[7]){
                 winner = gameBoard[1];
-                winnerMessage += `The winner is: ${winner}`;
+                winnerMessage += `Winner: ${winner}`;
                 $('.turn').addClass('ghost');
             } else if (gameBoard[2]!=null && gameBoard[2]===gameBoard[5] && gameBoard[5]===gameBoard[8]){
                 winner = gameBoard[2];
-                winnerMessage += `The winner is: ${winner}`;
+                winnerMessage += `Winner: ${winner}`;
                 $('.turn').addClass('ghost');
             } else if (gameBoard[0]!=null && gameBoard[0]===gameBoard[4] && gameBoard[4]===gameBoard[8]){
                 winner = gameBoard[0];
-                winnerMessage += `The winner is: ${winner}`;
+                winnerMessage += `Winner: ${winner}`;
                 $('.turn').addClass('ghost');
             } else if (gameBoard[6]!=null && gameBoard[6]===gameBoard[4] && gameBoard[4]===gameBoard[2]){
                 winner = gameBoard[6];
-                winnerMessage += `The winner is: ${winner}`;
+                winnerMessage += `Winner: ${winner}`;
                 $('.turn').addClass('ghost');
             } else if (gameBoard[0]!=null && gameBoard[1]!=null && gameBoard[2]!=null && gameBoard[3]!=null && gameBoard[4]!=null && gameBoard[5]!=null && gameBoard[6]!=null && gameBoard[7]!=null && gameBoard[8]!=null){
                 winnerMessage += `It's a draw!`;
