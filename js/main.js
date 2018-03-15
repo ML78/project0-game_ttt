8310c4d262b56ecf678904c3ed2b2616a40e1468
Monique Lee <moniquelee.7878@gmail.com>
2018-03-15 20:06:08 +1100

    14th cut - day 4

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
+//render game board by adding classes to the table & td HTML elements
 const renderBoard = function () {
-    $('table').addClass('grid');
-    $('td').addClass('box');
-    $('.turn').hide();
-  };
+  $('table').addClass('grid');
+  $('td').addClass('box');
+  $('.turn').hide();
+};
 
 let playerX = "X";
 let playerO = "O";
@@ -13,92 +14,97 @@ let gameBoard = [];
 $(document).ready(function() {
   renderBoard();
 
+  //when the player clicks on the X token, the starting player is set to X
   $('#crosses').on('click', function(){
-      player = playerX
-      $('span').text(player);
-      $('.turn').show();
-      });
+    player = playerX;
+    $('span').text(player);
+    $('.turn').show();
+  });
 
-    $('#noughts').on('click', function(){
-      player = playerO;
-      $('span').text(player);
-      $('.turn').show();
-    });
+  //when the player clicks on the O token, the starting player is set to O
+  $('#noughts').on('click', function(){
+    player = playerO;
+    $('span').text(player);
+    $('.turn').show();
+  });
 
-
+  //when the player clicks on a box in the game board, it displays the player's token in that box
+  //the #id of the box that is clicked is set as the index in the gameBoard array to which the player's token is added
   $('.box').on('click', function(){
-      $(this).text(player);
-      gameBoard[this.id] = player;
+    $(this).text(player);
+    gameBoard[this.id] = player;
 
-      if(player == playerX) {
-        nextMove = true;
-      } else if (player == playerO) {
-        nextMove = false;
-      }
+    //the player is switched by assigning a boolean value to the nextMove variable
+    if (player == playerX) {
+      nextMove = true;
+    } else if (player == playerO) {
+      nextMove = false;
+    }
 
-      if (nextMove){
-        $('span').text(playerO);
-        player = playerO;
-      } else {
-        $('span').text(playerX);
-        player = playerX;
-      }
+    //displays which player takes the next turn
+    if (nextMove){
+      $('span').text(playerO);
+      player = playerO;
+    } else {
+      $('span').text(playerX);
+      player = playerX;
+    }
 
-      let checkWin = function(){
-          let winnerMessage = "";
-          let winner;
+    //checks for win or draw by comparing the combinations of #ids (by rows, columns & diagonals) against the values in the gameBoard array
+    let checkWin = function(){
+      let winnerMessage = "";
+      let winner;
 
-          for(i=0; i<gameBoard.length; i++){
-            if(gameBoard[0]!=null && gameBoard[0]===gameBoard[1] && gameBoard[1]===gameBoard[2]){
-                winner = gameBoard[0];
-                winnerMessage += `Winner: ${winner}`;
-                $('.turn').addClass('ghost');
-            } else if(gameBoard[3]!=null && gameBoard[3]===gameBoard[4] && gameBoard[4]===gameBoard[5]){
-                winner = gameBoard[3];
-                winnerMessage += `Winner: ${winner}`;
-                $('.turn').addClass('ghost');
-            } else if(gameBoard[6]!=null && gameBoard[6]===gameBoard[7] && gameBoard[7]===gameBoard[8]){
-                winner = gameBoard[6];
-                winnerMessage += `Winner: ${winner}`;
-                $('.turn').addClass('ghost');
-            } else if (gameBoard[0]!=null && gameBoard[0]===gameBoard[3] && gameBoard[3]===gameBoard[6]){
-                winner = gameBoard[0];
-                winnerMessage += `Winner: ${winner}`;
-                $('.turn').addClass('ghost');
-            } else if (gameBoard[1]!=null && gameBoard[1]===gameBoard[4] && gameBoard[4]===gameBoard[7]){
-                winner = gameBoard[1];
-                winnerMessage += `Winner: ${winner}`;
-                $('.turn').addClass('ghost');
-            } else if (gameBoard[2]!=null && gameBoard[2]===gameBoard[5] && gameBoard[5]===gameBoard[8]){
-                winner = gameBoard[2];
-                winnerMessage += `Winner: ${winner}`;
-                $('.turn').addClass('ghost');
-            } else if (gameBoard[0]!=null && gameBoard[0]===gameBoard[4] && gameBoard[4]===gameBoard[8]){
-                winner = gameBoard[0];
-                winnerMessage += `Winner: ${winner}`;
-                $('.turn').addClass('ghost');
-            } else if (gameBoard[6]!=null && gameBoard[6]===gameBoard[4] && gameBoard[4]===gameBoard[2]){
-                winner = gameBoard[6];
-                winnerMessage += `Winner: ${winner}`;
-                $('.turn').addClass('ghost');
-            } else if (gameBoard[0]!=null && gameBoard[1]!=null && gameBoard[2]!=null && gameBoard[3]!=null && gameBoard[4]!=null && gameBoard[5]!=null && gameBoard[6]!=null && gameBoard[7]!=null && gameBoard[8]!=null){
-                winnerMessage += `Draw!`;
-                $('.turn').addClass('ghost');
-            } else {
-              winnerMessage += "";
-            }
-            return winnerMessage;
-          }
+      for(let i=0; i<gameBoard.length; i++){
+        if(gameBoard[0]!=null && gameBoard[0]===gameBoard[1] && gameBoard[1]===gameBoard[2]){
+          winner = gameBoard[0];
+          winnerMessage += `Winner: ${winner}`;
+          $('.turn').addClass('ghost');
+        } else if(gameBoard[3]!=null && gameBoard[3]===gameBoard[4] && gameBoard[4]===gameBoard[5]){
+          winner = gameBoard[3];
+          winnerMessage += `Winner: ${winner}`;
+          $('.turn').addClass('ghost');
+        } else if(gameBoard[6]!=null && gameBoard[6]===gameBoard[7] && gameBoard[7]===gameBoard[8]){
+          winner = gameBoard[6];
+          winnerMessage += `Winner: ${winner}`;
+          $('.turn').addClass('ghost');
+        } else if (gameBoard[0]!=null && gameBoard[0]===gameBoard[3] && gameBoard[3]===gameBoard[6]){
+          winner = gameBoard[0];
+          winnerMessage += `Winner: ${winner}`;
+          $('.turn').addClass('ghost');
+        } else if (gameBoard[1]!=null && gameBoard[1]===gameBoard[4] && gameBoard[4]===gameBoard[7]){
+          winner = gameBoard[1];
+          winnerMessage += `Winner: ${winner}`;
+          $('.turn').addClass('ghost');
+        } else if (gameBoard[2]!=null && gameBoard[2]===gameBoard[5] && gameBoard[5]===gameBoard[8]){
+          winner = gameBoard[2];
+          winnerMessage += `Winner: ${winner}`;
+          $('.turn').addClass('ghost');
+        } else if (gameBoard[0]!=null && gameBoard[0]===gameBoard[4] && gameBoard[4]===gameBoard[8]){
+          winner = gameBoard[0];
+          winnerMessage += `Winner: ${winner}`;
+          $('.turn').addClass('ghost');
+        } else if (gameBoard[6]!=null && gameBoard[6]===gameBoard[4] && gameBoard[4]===gameBoard[2]){
+          winner = gameBoard[6];
+          winnerMessage += `Winner: ${winner}`;
+          $('.turn').addClass('ghost');
+        } else if (gameBoard[0]!=null && gameBoard[1]!=null && gameBoard[2]!=null && gameBoard[3]!=null && gameBoard[4]!=null && gameBoard[5]!=null && gameBoard[6]!=null && gameBoard[7]!=null && gameBoard[8]!=null){
+          winnerMessage += `Draw!`;
+          $('.turn').addClass('ghost');
+        } else {
+          winnerMessage += "";
         }
+        return winnerMessage;
+      }
+    };
 
-      var score = checkWin();
+    //displays a message if there is a win or draw
+    var score = checkWin();
+    $('.score').show();
+    $('.score').text(score);
+  });
 
-
-        $('.score').show();
-        $('.score').text(score);
-    });
-
-
+  //when the restart button is clicked the game is reset by clearing values added during the game
   $('.restart').on('click', function(){
     $('td').text('');
     $('span').html('');
@@ -106,8 +112,6 @@ $(document).ready(function() {
     $('.turn').removeClass('ghost');
     player = "";
     gameBoard = [];
-
-
-    });
+  });
 
 });
